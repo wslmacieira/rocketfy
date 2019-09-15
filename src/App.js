c4578afc13a58df49dcda3fd10fe4d7ff87aab47
wslmacieira <wslmacieira@outlook.com>
2019-09-15 01:03:06 -0300

    adding components Board - Header - Card - List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global'
+import Header from './components/Header'
+import Board from './components/Board'
+
 function App() {
   return (
-    <div className="App"></div>
+    <>
+    <GlobalStyle />
+    <Header />
+    <Board />
+    </>
   );
 }
 
