a3225af5eef0b0ba8da4a1abc899dbd50fc1f73a
wslmacieira <wslmacieira@outlook.com>
2019-09-16 20:17:10 -0300

    adding drag n' drop

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -6,21 +6,26 @@ import Card from '../Card'
 
 import { Container } from './styles';
 
-export default function List() {
+export default function List({ data, index: listIndex }) {
   return (
-    <Container>
+    <Container done={data.done}>
       <header>
-        <h2>Tarefas</h2>
-        <button type="button">
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
           <MdAdd size={24} color="#fff" />
         </button>
+        )}
       </header>
       <ul>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {data.cards.map((card, index) => (
+        <Card 
+        key={card.id} 
+        listIndex={listIndex}
+        index={index} 
+        data={card} 
+        />)
+        )}
       </ul>
     </Container>
   );
